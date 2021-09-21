77fb2577f44487f7d22578e8ef31db8a79ed84d2
Nitin kalra <nikkalra88@gmail.com>
2021-09-21 05:20:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,115 @@
+var readlineSync = require('readline-sync');
+var score = 0
+
+var topScores = 
+[
+{
+
+userName: 'Nitin',
+score: 3,
+
+},
+
+]
+
+
+console.log("Welcome to HOW WELL DO YOU KNOW BOLLYWOOD");
+
+var userName = readlineSync.question("What's Your Name? ");
+
+var response = readlineSync.keyInYN("Are you ready to play? ");
+
+
+
+
+var questions = 
+[
+{
+
+question: "Whos's the king khan of bolyywood?(Hint: 3 word answer) ",
+answer: "SRK"
+
+},
+
+{
+
+question: "Where is the biggest film city located ",
+answer: "Noida"
+
+
+},
+
+{
+
+question: "The film to be shown in theatre for the longest time? (Hint: 4 word answer) ",
+answer: "DDLJ"
+
+},
+]
+
+function Play(question,answer){
+
+var input = readlineSync.question(question)
+
+if (input.toUpperCase() === answer.toUpperCase())
+{
+
+console.log("That's right")
+score += 1
+console.log("Your current score is ", score)
+
+}
+else{
+
+console.log("That's incorrect")
+console.log("Your current score is ", score)
+
+}
+
+}
+
+function game() {
+
+
+  if (response)
+
+{
+
+console.log("Aight! Let's Begin");
+
+
+
+for (var i=0; i<questions.length; i++ )
+{
+
+Play(questions[i].question,questions[i].answer)
+
+}
+
+console.log("You've completed the game \nYour final score is ", score)
+
+if (score>=topScores[0].score)
+{
+
+console.log("You've created a highscore, Send me a Screenshot so that I can update")
+console.log("Previous high score was \n", topScores[0].userName, '-', topScores[0].score)
+
+}
+else
+{
+console.log("Check the highscores \n", topScores[0].userName, '-', topScores[0].score)
+
+}
+}
+else
+{
+
+console.log("Oops! that's not a yes, lagta hai humara saath yahi tak tha (^_^) \n\nExiting, click on the play button at bottom right if you change your mind and decide to play")
+
+}
+
+
+
+}
+
+game()
